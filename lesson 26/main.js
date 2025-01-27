b78e5eaba1af444e6f3791fd986f4c1ad2953712
avdyl123 <avdylzeqiri732@gmail.com>
2025-01-27 03:19:17 -0800

    a
    a

---
+++ lesson 26/main.js
@@ -0,0 +1,55 @@
+console.log("Goodbye");
+
+var x=5;
+
+var a = 50 + 50;
+console.log(a);
+
+var b=20;
+var c=30;
+var d=b+c;
+console.log(d);
+
+
+var e =40;
+var f=10;
+var g= e-f;
+console.log(g),
+
+var h=100;
+var i =5;
+var j=h/i;
+console.log(j);
+
+var n=99;
+n++;
+console.log(n);
+
+var o= 101;
+o--;
+console.log(o);
+
+var p=20;
+varq= p**2;
+console.log(q);
+
+var x = 15;
+x+=5;
+console.log(x);
+
+var s=10;
+s*=7;
+console.log(s);
+
+var v=5;
+var w=5
+console.log(v == w)
+
+
+var z=5;
+var t=5;
+console.log(z != t)
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 
