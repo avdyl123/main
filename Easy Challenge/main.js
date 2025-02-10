5969b1314eec481714a5029b0ec95887f33605d7
avdyl123 <avdylzeqiri732@gmail.com>
2025-02-10 03:18:24 -0800

    a
    a

---
+++ Easy Challenge/main.js
@@ -0,0 +1,10 @@
+var friends=['ylli' , 'dorina' , 'orges' 'art' , 'enea']
+
+
+console.log(friends);
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+
+friends.unshift('enea');
